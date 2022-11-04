95778a3e5116527f7b4c3ba098ce16dd2eca5929
Saar Berenstein <saarbm25@gmail.com>
2022-11-05 00:51:15 +0200

    playing with basic animations

--- src/screens/SearchScreen.js
+++ src/screens/SearchScreen.js
@@ -1,12 +1,63 @@
-import {View, Text, StyleSheet} from 'react-native';
-import React from 'react';
+import {
+  View,
+  Text,
+  StyleSheet,
+  Animated,
+  Easing,
+  useWindowDimensions,
+} from 'react-native';
+import React, {useEffect, useState, useRef} from 'react';
+import {colors} from '../global/styles';
 
 export default function SearchScreen() {
+  const dim = useWindowDimensions();
+  const width = dim.width;
+  const height = dim.height;
+  const touch = useRef(
+    new Animated.ValueXY({x: width / 2, y: height / 2}),
+  ).current;
+
   return (
-    <View>
-      <Text>SearchScreen</Text>
+    <View style={styles.container}>
+      <Animated.View
+        onStartShouldSetResponder={() => true}
+        onResponderRelease={() => {
+          Animated.spring(touch, {
+            toValue: {
+              y: height / 2,
+              x: width / 2,
+            },
+            useNativeDriver: false,
+          }).start();
+        }}
+        onResponderMove={event => {
+          touch.setValue({
+            x: event.nativeEvent.pageX,
+            y: event.nativeEvent.pageY,
+          });
+          // console.log(touch)
+        }}
+        style={[
+          styles.squre,
+          {
+            position: 'absolute',
+            left: Animated.subtract(touch.x, 25),
+            top: Animated.subtract(touch.y, 25),
+          },
+        ]}></Animated.View>
     </View>
   );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  squre: {
+    width: 50,
+    height: 50,
+    backgroundColor: colors.button,
+  },
+});
